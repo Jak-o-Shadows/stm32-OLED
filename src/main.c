70c6aba6825b46672d77753c3bb45915f8f7a359
Jak_o_Shadows <jak-o-shadows@users.noreply.github.com>
2020-02-15 15:58:58 +1030

    Pixel Set/Clear functions

--- src/main.c
+++ src/main.c
@@ -10,6 +10,14 @@
 #include <stdint.h>
 #include <string.h>
 
+// Older Bits
+
+// Define Functions
+#define IsHigh(BIT, PORT) ((PORT & (1 << BIT)) != 0)
+#define IsLow(BIT, PORT) ((PORT & (1 << BIT)) == 0)
+#define SetBit(BIT, PORT) PORT |= (1 << BIT)
+#define ClearBit(BIT, PORT) PORT &= ~(1 << BIT)
+
 typedef enum Status_e
 {
     STATUSok = 0,
@@ -223,13 +231,74 @@ Status init(SSD1306 *dev)
     return STATUSok;
 }
 
-// Older Bits
+// Graphics Drawing Functions
 
-// Define Functions
-#define IsHigh(BIT, PORT) ((PORT & (1 << BIT)) != 0)
-#define IsLow(BIT, PORT) ((PORT & (1 << BIT)) == 0)
-#define SetBit(BIT, PORT) PORT |= (1 << BIT)
-#define ClearBit(BIT, PORT) PORT &= ~(1 << BIT)
+Status lookupPixelLocation(uint8_t *byteIdx, uint8_t *bitIdx, uint8_t segmentStart_x, uint8_t segmentStart_y, uint8_t segmentSize_x, uint8_t segmentSize_y, uint8_t pixel_x, uint8_t pixel_y)
+{
+    // First check if the pixel is within the segment
+    if (((pixel_x >= segmentStart_x) && (pixel_x < (segmentStart_x + segmentSize_x))) &&
+        ((pixel_y >= segmentStart_y) && (pixel_y < (segmentStart_y + segmentSize_y))))
+    {
+        // Continue proceccessing
+    }
+    else
+    {
+        return STATUSbad;
+    }
+
+    // The pixel is within the display area set by the buffer - figure
+    //  out where to set
+
+    // First determine which pixel buffer byte the pixel is in
+    //  Now, the bytes go x, then y. Always.
+    uint8_t local_x = pixel_x - segmentStart_x;
+    uint8_t local_y = pixel_y - segmentStart_y;
+    uint8_t byteColumnIndex = local_x / 8;
+    uint8_t byteRowIndex = local_y;
+    *byteIdx = (byteRowIndex * segmentSize_x / 8) + byteColumnIndex;
+    *bitIdx = local_x - byteColumnIndex * 8;
+
+    return STATUSok;
+}
+
+Status pixelSet(Segment_t *buffer, uint8_t segmentStart_x, uint8_t segmentStart_y, uint8_t segmentSize_x, uint8_t segmentSize_y, uint8_t pixel_x, uint8_t pixel_y)
+{
+    // Set a pixel in the segment, if applicable
+
+    uint8_t byteIdx;
+    uint8_t bitIdx;
+    Status sts = lookupPixelLocation(&byteIdx, &bitIdx, segmentStart_x, segmentStart_y, segmentSize_x, segmentSize_y, pixel_x, pixel_y);
+
+    if (sts == STATUSok)
+    {
+        SetBit(bitIdx, buffer->components.pixelBuffer[byteIdx]);
+
+        return STATUSok;
+    }
+    else
+    {
+        return sts;
+    }
+}
+
+Status pixelClear(Segment_t *buffer, uint8_t segmentStart_x, uint8_t segmentStart_y, uint8_t segmentSize_x, uint8_t segmentSize_y, uint8_t pixel_x, uint8_t pixel_y)
+{
+    // Clear a pixel in the segment, if applicable
+    uint8_t byteIdx;
+    uint8_t bitIdx;
+    Status sts = lookupPixelLocation(&byteIdx, &bitIdx, segmentStart_x, segmentStart_y, segmentSize_x, segmentSize_y, pixel_x, pixel_y);
+
+    if (sts == STATUSok)
+    {
+        ClearBit(bitIdx, buffer->components.pixelBuffer[byteIdx]);
+
+        return STATUSok;
+    }
+    else
+    {
+        return sts;
+    }
+}
 
 void clock_setup(void)
 {
@@ -565,8 +634,12 @@ void dma1_channel4_isr(void)
 
     // Then process next buffer
     Segment_t *nextBuffer = buffers[segmentIdx];
-    memset(nextBuffer->components.pixelBuffer, segmentIdx, segmentPixelCount / 8);
+    memset(nextBuffer->components.pixelBuffer, 0xFF, segmentPixelCount / 8);
 
+    for (uint8_t col = 10; col < 30; col++)
+    {
+        pixelClear(nextBuffer, 0, segment, 32, 1, 6, col);
+    }
     // Artificial delay for development
     for (int i = 0; i < 100000; i++)
     {
